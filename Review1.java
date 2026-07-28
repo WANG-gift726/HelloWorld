fd35eb718ee2154b0933966655015129df48410a
WANG-gift726 <wpq159951@163.com>
2026-07-28 15:25:33 +0800

    Add files via upload

---
+++ Review1.java
@@ -0,0 +1,25 @@
+public class Review1 {
+    public static void main(String[] args){
+        int age = 18;
+        double price = 23.3;
+        char male = '男';
+        boolean pass = true;
+        String name = "WANG";
+        System.out.println("age = " + age);
+        System.out.println("price = " + price);
+        System.out.println("male = " + male);
+        System.out.println("pass = " + pass);
+        System.out.println("name = " + name);
+
+        int x = 5;
+        int y = x++;
+        System.out.println(x);
+        System.out.println(y);
+        int m = 5;
+        int n = ++m;
+        System.out.println(m);
+        System.out.println(n);
+
+
+    }
+}
