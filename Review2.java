dcb655f5d7d505f2615d414d5f3df30e99518ee6
WANG-gift726 <wpq159951@163.com>
2026-08-01 00:38:12 +0800

    Add files via upload

---
+++ Review2.java
@@ -0,0 +1,14 @@
+package base;
+
+public class Review2 {
+    public static void main(String[] args){
+        String name = "WANG";
+        int age = 18;
+        double tall = 174;
+        System.out.println("名字:" + name + "年龄:" + age + "身高:" + tall);
+
+        double price = 99.0;
+        System.out.println((int)price);
+
+    }
+}
