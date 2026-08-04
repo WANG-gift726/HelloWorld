c67e1d4243d393d67ab7fed90e4f6b3a15a4e824
WANG-gift726 <wpq159951@163.com>
2026-08-04 22:55:24 +0800

    Add files via upload

---
+++ IndependentLoop.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class IndependentLoop {
+    public static void main(String[] agrs){
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + "");
+            if(i % 3 == 0){
+                System.out.println();
+            }
+        }
+    }
+}
---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+package Loop;
+
+public class MultiplicationTable {
+    public static void main(String[] args){
+        for (int j = 1; j <= 9; j++) {
+            for (int i = 1; i <= j; i++) {
+                System.out.print(j + "*" + i + "=" + (j*i) + "\t");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
