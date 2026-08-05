bc7ea1c00c193a1fc03b7afec6f76d7d268ae2b3
WANG-gift726 <wpq159951@163.com>
2026-08-06 00:36:05 +0800

    Add files via upload

---
+++ Triangle2.java
@@ -0,0 +1,16 @@
+package Loop;
+
+public class Triangle2 {
+    public static void main(String[] args){
+        int rows = 9;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("i");
+            }
+            System.out.println();
+        }
+    }
+}
